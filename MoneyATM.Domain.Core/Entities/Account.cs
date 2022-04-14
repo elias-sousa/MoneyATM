de9d377fdea00e20e339a8fcfd2567824f364a1b
Elias Rodrigues de Sousa <e.sousa@outlook.com.br>
2022-04-13 21:41:13 -0300

    Ajuste encapsulamento

--- MoneyATM.Domain.Core/Entities/Account.cs
+++ MoneyATM.Domain.Core/Entities/Account.cs
@@ -11,10 +11,10 @@ namespace MoneyATM.Domain.Core.Entities
     /// </summary>
     public class Account
     {
-        protected int AccountNumber { get; set; }
-        protected int Pin { get; set; }
-        protected decimal AvailableBalance { get; set; }
-        protected decimal TotalBalance { get; set; }
+        public int AccountNumber { get; set; }
+        public int Pin { get; set; }
+        public decimal AvailableBalance { get; set; }
+        public decimal TotalBalance { get; set; }
 
         public Account(int theAccountNumber, int thePIN,
            decimal theAvailableBalance, decimal theTotalBalance)
